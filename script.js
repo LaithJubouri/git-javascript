e0429d6fe54e61bfd4c57f1cb59e78369a2d98e2
unknown <jubo0002@algonquinlive.com>
2020-01-15 11:28:25 -0500

    javascript file

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = "Laith"
+console.log(`Hello, my name is ${name}`)
+
+let number = 5
+let result = number * 2
+let result1 = result * 5
+let result2 = result1 / number
+console.log(number*2)
+console.log(result*5)
+console.log(result1/number)
+console.log(result2-7)
